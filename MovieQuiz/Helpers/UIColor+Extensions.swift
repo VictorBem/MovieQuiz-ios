f02a865ddbfa7153661ded21a68549e7268fa3d0
Victor <b3m.v@yandex.ru>
2024-04-03 23:29:07 +0300

    feat: Добавлен экран запуска приложения, основной экран и логика работы приложения

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor { 
+    static var ypBlack: UIColor { UIColor(named: "YS Black") ?? UIColor.black }
+    static var ypRed: UIColor { UIColor(named: "YS Red") ?? UIColor.red }
+    static var ypGreen: UIColor { UIColor(named: "YS Green") ?? UIColor.green }
+    static var ypBackground: UIColor { UIColor(named: "YS Background") ?? UIColor.darkGray }
+    static var ypGray: UIColor { UIColor(named: "YS Gray") ?? UIColor.gray }
+    static var ypWhite: UIColor { UIColor(named: "YS White") ?? UIColor.white}
+}
