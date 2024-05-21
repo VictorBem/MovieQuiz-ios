b528545127214a6ce967018978e987c749a2194e
Victor <b3m.v@yandex.ru>
2024-05-21 16:57:11 +0300

    fix: убрал private у convert для прохождения теста

--- MovieQuiz/Presenter/MovieQuizPresenter.swift
+++ MovieQuiz/Presenter/MovieQuizPresenter.swift
@@ -73,7 +73,7 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
         correctAnswers += 1
     }
     
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+    func convert(model: QuizQuestion) -> QuizStepViewModel {
         return QuizStepViewModel(image: UIImage(data: model.image) ?? UIImage(),
                                  question: model.text,
                                  questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
